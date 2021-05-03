e677da06c1a37112bf0b7965e18ed84adf900510
Movimento92 <77988662+Movimento92@users.noreply.github.com>
2021-05-03 15:35:17 +0300

    Create prr.js

---
+++ prr.js
@@ -0,0 +1,7 @@
+function checkStorage(available, ordered) {
+    let message;
+    // Write your code under this line
+      let message = $ordered > $available?'Not enough goods in stock!':'The order is placed, the manager will contact you';
+     // Write your code above this line
+    return message;
+  }
